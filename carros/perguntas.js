453e816f40b9c25632e28255169f0f4d697d6d11
Amanda <amanda.leite10@escola.pr.gov.br>
2025-10-07 07:45:12 -0300

    Add files via upload

---
+++ carros/perguntas.js
@@ -0,0 +1,72 @@
+criaCartao(
+    'Carros',
+    'Qual é a função de um motor??',
+    'Ele gera força para movimentar o carro'
+)
+
+criaCartao(
+    'Carros',
+    'O que é ABS?',
+    'É um sistema que evita o travamento dos freios'
+)
+
+criaCartao(
+    'Carros',
+    'Para que serve o câmbio?',
+    'Controlar a força e velocidade do carro '
+)
+
+criaCartao(
+    'Carros',
+    'O que significa SUV',
+    ' Veículo utilitário esportivo '
+)
+
+criaCartao(
+    'Carros',
+    'Qual o combustível mais comum no Brasil',
+    'Gasolina e etanol '
+)
+
+criaCartao(
+    'Carros',
+    'O que faz o radiador',
+    '  Resfria o motor  '
+)
+
+criaCartao(
+    'Carros',
+    ' O que é um carro híbrido?',
+    ' Usa motor elétrico e a combustão'
+)
+
+criaCartao(
+    'Carros',
+    ' Qual é a função da embreagem?',
+    ' Conecta ou desconecta o motor do câmbio'
+)
+
+criaCartao(
+    'Carros',
+    'O que é um hatch',
+    ' Carro com porta-malas integrado á cabine'
+)
+
+criaCartao(
+    'Carros',
+    ' Para que serve o óleo do motor?',
+    'Lubrificar e proteger as peças internas '
+)
+
+criaCartao(
+    'Carros',
+    'O que é direção hidráulica?',
+    'Facilita virar o volante com menos esforço'
+)
+
+criaCartao(
+    'Carros',
+    ' O que indica o velocímetro?',
+    'A velocidade do carro '
+)
+
